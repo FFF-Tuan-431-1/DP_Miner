0ed7d56eeae8a7fa14b87953c1a8443f9b7a7af5
lyk <1756633219@qq.com>
2015-05-21 22:26:52 +0800

    哈哈

--- miner.cpp
+++ miner.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int size ;
 int gold[1000][1000] ;
 int maxGold[1000][1000] ;
-bool route[1000][1000];
+int route[1000][1000];
 
 int getMaxGold(){
 
@@ -16,11 +16,11 @@ int getMaxGold(){
 //            maxGold[i][j] = max(gold[i][j] + maxGold[i + 1][j], gold[i][j] + maxGold[i + 1][j + 1]);
             if (maxGold[i + 1][j] > maxGold[i + 1][j + 1]) {
                 maxGold[i][j] = gold[i][j] + maxGold[i + 1][j];
-                route[i][j] = false;
+                route[i][j] = 0 ;
             }
             else{
                 maxGold[i][j] = gold[i][j] + maxGold[i + 1][j + 1] ;
-                route[i][j] = true ;
+                route[i][j] = 1 ;
             }
         }
     }
@@ -29,19 +29,19 @@ int getMaxGold(){
 
 
 void printRoute(){
-    int cur = 1 ;
+    int current = 1 ;
     for (int i = 1;i < size;i++){
-        cout << gold[i][cur] <<"->" ;
-        cur += route[i][cur] ;
+        cout << gold[i][current] <<"->" ;
+        current += route[i][current] ;
     }
-    cout << gold[size][cur] << endl ;
+    cout << gold[size][current] << endl ;
 }
 
 int main() {
     int maxnum ;
-    cout <<"请输入金字塔层数最少为1层" ;
+    cout <<"请输入金字塔层数（最少为1层）：" ;
     cin >> size;
-    cout << "请输入每个砖块中的钻石数目" ;
+    cout << "请输入每个砖块中的钻石数目：" ;
     for (int i = 1;i <= size;i++){
         for (int j = 1;j <= i;j++){
             cin >> gold[i][j] ;
